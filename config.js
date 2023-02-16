c351b7f8d333edb8acb3dd8be7ba37e84d82b990
RidOne673 <76752207+RidOne673@users.noreply.github.com>
2023-02-16 19:21:26 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-global.teleApi = ''
-global.gptApi = ''
+global.teleApi = '5718876282:AAEx3OZBkr4EIJybGzR-Uhhp4cfQAmym118'
+global.TTteleApi = '6201791036:AAHoRovShULXomyU4kZaKWsry2t0QNojcZw'
+global.gptApi = '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'
 global.ownId = '1005747269'
 
 global.donate = {
@@ -11,4 +12,11 @@ global.donate = {
 	qris : "https://i.ibb.co/W0kSRXL/MBRC-1675416263714.png"
 }
 
-global.badword = ["scrape","bunuh","kontol","memek","fuck","ngewe","racun","vagina","penis","sex","bangsat","mencuri","tete","bom","nigga","negro","slot","bobol","hack","kawin","ngentot","ngentod","ngntd","ngntt","etubuh"];
+global.moreBot = `Octave memiliki beberapa jenis bot sebagai berikut :
+
+ChatGPT bot: @octv_chatgpt_bot
+Tiktok Downloader bot: @octv_tiktokdownloader_bot
+
+`
+
+global.badword = ["scrape","bunuh","kontol","memek","fuck","ngewe","racun","vagina","penis","sex","bangsat","mencuri","tete","bom","nigga","negro","slot","bobol","hack","kawin","ngentot","ngentod","ngntd","ngntt","etubuh","lotre","lottery"];
